205e739f52f77170e67d69fc60e8023a1af33138
pojkvasker <philipahl@gmail.com>
2019-12-06 09:10:22 +0100

    some changes

--- some_plots.py
+++ some_plots.py
@@ -47,12 +47,12 @@ data_sw = pd.read_csv('processed.switzerland_2.txt',\
 data_va = pd.read_csv('processed.va_2.txt',\
                       sep=",", header=None)
 # Merge all datasets into one dataset
-data = pd.concat([data_cl,data_hu,data_sw,data_va]).replace('?', np.nan).reset_index(drop=True)
+data_cl = pd.concat([data_cl]).replace('?', np.nan).reset_index(drop=True)
 # Column names, according to the web source
-data.columns = ['age','sex','cp','trestbps','chol','fbs','restecg',\
+data_cl.columns = ['age','sex','cp','trestbps','chol','fbs','restecg',\
                 'thalach','exang','oldpeak','slope','ca','thal','num']
 # Filter out datapoints with missing values
-filtered_data = data.dropna()
+filtered_data = data_cl.dropna()
 
 sick = filtered_data.loc[filtered_data['num'] == 1]
 good = filtered_data.loc[filtered_data['num'] == 0]
@@ -75,40 +75,47 @@ label = filtered_data['num'].astype(bool).astype(int).values.tolist()       # pr
 
 plt.style.use('seaborn-white')
 kwargs = dict(histtype='step', alpha=0.3, density=True, ec="k")
-bins = 20
+bins = len(filtered_data)
+print('bins'+str(bins))
 int_discr = {}
 
 plt.figure(1)
+bins = np.linspace(20, 80, 25)
 plt.xlabel('Age')
 age_s = plt.hist(sick['age'], bins=bins, **kwargs, edgecolor='red')
 age_g = plt.hist(good['age'], bins=bins, **kwargs, edgecolor='green')
 int_discr["age"] = intrinsic_discrepancy(age_s[0],age_g[0])
 
 plt.figure(2)
+bins = 2
 plt.xlabel('Gender (1 = male, 0 = female)')
 sex_s = plt.hist(sick['sex'], bins=bins, **kwargs, edgecolor='red')
 sex_g = plt.hist(good['sex'], bins=bins, **kwargs, edgecolor='green')
 int_discr["sex"] = intrinsic_discrepancy(sex_s[0],sex_g[0])
 
 plt.figure(3)
+bins = 4
 plt.xlabel(' chest pain type 1: typical angina, 2: atypical angina, 3: non-anginal pain, 4: asymptomatic')
 cp_s = plt.hist(sick['cp'], bins=bins, **kwargs, edgecolor='red')
 cp_g = plt.hist(good['cp'], bins=bins, **kwargs, edgecolor='green')
 int_discr["cp"] = intrinsic_discrepancy(cp_s[0],cp_g[0])
 
 plt.figure(4)
+bins = np.linspace(80, 200, 15)
 plt.xlabel('Resting blood pressure (in mm Hg)')
 bp_s = plt.hist(sick['trestbps'].astype(int), bins=bins, **kwargs, edgecolor='red')
 bp_g = plt.hist(good['trestbps'].astype(int), bins=bins, **kwargs, edgecolor='green')
 int_discr["bp"] = intrinsic_discrepancy(bp_s[0],bp_g[0])
 
 plt.figure(5)
+bins = np.linspace(110, 200, 15)
 plt.xlabel('serum cholestoral in mg/dl')
 chol_s = plt.hist(sick['chol'].astype(int), bins=bins, **kwargs, edgecolor='red')
 chol_g = plt.hist(good['chol'].astype(int), bins=bins, **kwargs, edgecolor='green')
 int_discr["chol"] = intrinsic_discrepancy(chol_s[0],chol_g[0])
 
 plt.figure(6)
+bins = 2
 plt.xlabel('Fasting blood sugar > 120 mg/dl  (1 = true, 0 = false)')
 fbs_s = plt.hist(sick['fbs'].astype(int), bins=bins, **kwargs, edgecolor='red')
 fbs_g = plt.hist(good['fbs'].astype(int), bins=bins, **kwargs, edgecolor='green')
@@ -121,39 +128,46 @@ ecg_g = plt.hist(good['restecg'].astype(bool).astype(int), bins=bins, **kwargs, 
 int_discr["ecg"] = intrinsic_discrepancy(ecg_s[0],ecg_g[0])
 
 plt.figure(8)
+bins = np.linspace(80, 200, 15)
 plt.xlabel('Maximum heart rate achieved')
 thalach_s = plt.hist(sick['thalach'].astype(int), bins=bins, **kwargs, edgecolor='red')
 thalach_g = plt.hist(good['thalach'].astype(int), bins=bins, **kwargs, edgecolor='green')
 int_discr["thalach"] = intrinsic_discrepancy(thalach_s[0],thalach_g[0])
 
 plt.figure(9)
+bins = 2
 plt.xlabel('Exercise induced angina (1 = yes 0 = no)')
 exang_s = plt.hist(sick['exang'].astype(int), bins=bins, **kwargs, edgecolor='red')
 exang_g = plt.hist(good['exang'].astype(int), bins=bins, **kwargs, edgecolor='green')
 int_discr["exang"] = intrinsic_discrepancy(exang_s[0],exang_g[0])
 
 plt.figure(10)
+bins = np.linspace(0, 6, 15)
 plt.xlabel('ST depression induced by exercise relative to rest')
 old_s = plt.hist(sick['oldpeak'].astype(float), bins=bins, **kwargs, edgecolor='red')
 old_g = plt.hist(good['oldpeak'].astype(float), bins=bins, **kwargs, edgecolor='green')
 int_discr["old"] = intrinsic_discrepancy(old_s[0],old_g[0])
 
 plt.figure(11)
+bins = 3
 plt.xlabel('The slope of the peak exercise ST segment (1 = upsloping, 2 = flat, 3 = downsloping)')
 slope_s =plt.hist(sick['slope'].astype(int), bins=bins, **kwargs, edgecolor='red')
 slope_g = plt.hist(good['slope'].astype(int), bins=bins, **kwargs, edgecolor='green')
 int_discr["slope"] = intrinsic_discrepancy(slope_s[0],slope_g[0])
 
 plt.figure(12)
+bina = 4
 plt.xlabel('Number of major vessels (0-3) colored by flourosopy')
 ca_s = plt.hist(sick['ca'].astype(float).astype(int), bins=bins, **kwargs, edgecolor='red')
 ca_g = plt.hist(good['ca'].astype(float).astype(int), bins=bins, **kwargs, edgecolor='green')
 int_discr["ca"] = intrinsic_discrepancy(ca_s[0],ca_g[0])
 
 plt.figure(13)
+bins = 3
 plt.xlabel('Thalassemia (3 = normal, 6 = fixed defect, 7 = reversable defect)')
 thal_s = plt.hist(sick['thal'].astype(float).astype(int), bins=bins, **kwargs, edgecolor='red')
 thal_g = plt.hist(good['thal'].astype(float).astype(int), bins=bins, **kwargs, edgecolor='green')
 int_discr["thal"] = intrinsic_discrepancy(thal_s[0],thal_g[0])
-
 print(int_discr)
+plt.show()
+
